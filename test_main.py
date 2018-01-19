06754ed725f67c3dc59460f12048aed27bf5d073
Sriharsha Sammeta <sriharsha4444@icloud.com>
2018-01-18 19:36:51 -0500

    adding print statement

--- test_main.py
+++ test_main.py
@@ -24,7 +24,8 @@ class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/get_author/ulysses')
-        assert rv.data == 'James Joyce'
+        print(rv.data)
+	assert rv.data == 'James Joyce'
 
 if __name__ == '__main__':
     unittest.main()
